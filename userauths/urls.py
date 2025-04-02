70f87b66b01113e4a9188fb51929f27e7e26844a
Anvarbek <ziyodovanvar1999@gmail.com>
2025-04-02 17:03:16 +0900

    Feat: develop sign-up page

--- userauths/urls.py
+++ userauths/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from userauths import views
-from django.contrib.auth import views as auth_views
 
-
-app_name ="userauths"
+app_name = "userauths"
 
 urlpatterns = [
     path("sign-up/", views.RegisterView, name="sign-up"),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'), 
-
+    path("sign-in/", views.LoginView, name="sign-in"),
+    path("sign-out/", views.logoutView, name="sign-out"),
 ]
