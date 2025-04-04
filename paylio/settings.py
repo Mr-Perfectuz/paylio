aa7d86b906b97177c02d75bc76971783b3744b38
Anvarbek <ziyodovanvar1999@gmail.com>
2025-04-04 09:41:57 +0900

    Feat: modify user account page

--- paylio/settings.py
+++ paylio/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+LOGIN_URL ="userauths:sign-in"
+# LOGIN_REDIRECT_URL = "userauths:sign-in"
+LOGOUT_REDIRECT_URL = "userauths:sign-in"
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'paylio', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')   
