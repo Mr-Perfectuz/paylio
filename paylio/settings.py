82c04115f330dc4d8ffb1445bfef24f57f5c3713
Anvarbek <ziyodovanvar1999@gmail.com>
2025-04-08 11:21:31 +0900

    Fix static files and whitenoise issue

--- paylio/settings.py
+++ paylio/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "paylio.urls"
@@ -135,12 +136,15 @@ LOGIN_URL ="userauths:sign-in"
 LOGOUT_REDIRECT_URL = "userauths:sign-in"
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'paylio', 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')   
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_ROOT = BASE_DIR / 'media'
 LOGOUT_REDIRECT_URL = '/admin/login/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
