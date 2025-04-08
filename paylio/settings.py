265b02515d175bdc516278797679ff3a397d20ce
Anvarbek <ziyodovanvar1999@gmail.com>
2025-04-08 09:00:43 +0900

    Fix ALLOWED_HOSTS for Railway

--- paylio/settings.py
+++ paylio/settings.py
@@ -26,8 +26,10 @@ SECRET_KEY = "django-insecure-&5arq9bhko6k2xk9c7qpt&1iyi7nh-i92le0-ekf*@j--pl3ul
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['paylio-app.up.railway.app', 'localhost', '127.0.0.1']
 
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
+
+#ALLOWED_HOSTS = ['paylio-app.up.railway.app', 'localhost', '127.0.0.1']/
 
 
 # Application definition
